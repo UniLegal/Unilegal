baeab23aab3358b0a696a9547687c84350460e6a
JackySo2000 <jackyso2000@gmail.com>
2020-12-05 23:23:57 +0800

    no message

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,27 +1,78 @@
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 import React from 'react'
-import Icon from './icon'
-import {cn} from '../lib/helpers'
-
+import { cn } from '../lib/helpers'
 import styles from './header.module.css'
+import logo from '../images/logo-unilegal-llc.png'
+import logomob from '../images/logo-unilegal-llcmob.png'
+import mystyles from './header.scss'
 
-const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
-  <div className={styles.root}>
+
+const Header = ({ onHideNav, onShowNav, showNav, siteTitle, siteImage }) => (
+    <div className={styles.root}>
+        <div class="mobile-container">
+        <div class="content">
+            <nav>
+                <input type="checkbox" id="hamburger1" />
+                <label for="hamburger1"></label>
+                <ul class="nav-links">
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about-us/'>About Us</Link>
+                    </li>
+                    <li>
+                        <Link to='/profile-of-lawyers/'>Profile of Lawyers</Link>
+            </li>
+            <li>
+              <Link to='/blogs/'>Blogs</Link>
+            </li>
+                    <li>
+                        <Link to='/contact/'>Contact Us</Link>
+                    </li>
+                </ul>
+                </nav>
+                <div class="logodiv">
+                <Link to='/'>
+                    <img className="logo"
+                            src={logomob}
+                            alt="portomono_logo"
+                            height="720"
+                            width="2500"
+                        /></Link>
+                </div>
+            </div>
+            </div>
     <div className={styles.wrapper}>
-      <div className={styles.branding}>
-        <Link to='/'>{siteTitle}</Link>
-      </div>
+      <h1 className={styles.branding}>
+        <Link to='/'>
+          <img className="logo"
+                        src={logo}
+          alt="portomono_logo"
+        /></Link>
+      </h1>
 
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
-      </button>
 
       <nav className={cn(styles.nav, showNav && styles.showNav)}>
+        <div className="headerbtn">
         <ul>
           <li>
-            <Link to='/archive/'>Archive</Link>
+            <Link to='/'>Home</Link>
           </li>
-        </ul>
+          <li>
+            <Link to='/about-us/'>About Us</Link>
+            </li>
+            <li>
+              <Link to='/profile-of-lawyers/'>Profile of Laywers</Link>
+            </li>
+            <li>
+              <Link to='/blogs/'>Blogs</Link>
+            </li>
+            <li>
+              <Link to='/contact/'>Contact Us</Link>
+            </li>
+          </ul>
+          </div>
       </nav>
     </div>
   </div>
